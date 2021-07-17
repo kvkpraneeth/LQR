6afac1181bd622446eb85cae76e97eece38cba11
kvkpraneeth <praneeth.pep@gmail.com>
2021-07-17 13:30:23 +0530

    Algebraic Riccati Equation Solution Required

--- include/lqr/lqr.h
+++ include/lqr/lqr.h
@@ -2,7 +2,7 @@
 #define LQR_H
 
 #include "eigen3/Eigen/Core"
-#include <eigen3/Eigen/src/Core/Matrix.h>
+#include <memory>
 
 template<int numberOfInputs, int numberOfOutputs, int numberOfStates>
 struct LinearStateSpace
@@ -13,13 +13,13 @@ struct LinearStateSpace
 
         X' = AX + BU; Y = KX;
 
-        X: numberOfStateVariables_ * 1 Vector
-        Y: numberOfOutputs_ * 1 Vector
-        U: numberOfInputs_ * 1 Vector
+        @X (numberOfStates, 1): Vector
+        @Y (numberOfOutputs, 1): Vector
+        @U (numberOfInputs, 1): Vector
 
-        A: weights/penalties of X
-        B: weights/penalties of U
-        K: weights/penalties of X with respect to the output equation.
+        @A weights/penalties of X
+        @B weights/penalties of U
+        @K System Gain for Full State Feedback Controller
     */
 
     Eigen::Matrix<double, numberOfInputs, 1> U;  
@@ -36,7 +36,7 @@ template<int numberOfInputs, int numberOfOutputs, int numberOfStates>
 class lqr
 {
 
-    //Default Constructor.
+    //Defaults.
     lqr();
 
     private:
@@ -45,10 +45,30 @@ class lqr
         int numberOfInputs_, numberOfOutputs_, numberOfStates_;
 
         //System init.
-        LinearStateSpace<numberOfInputs, numberOfOutputs, numberOfStates>* System;
+        struct LinearStateSpace<numberOfInputs, numberOfOutputs, numberOfStates> System;
 
         //Cost Function Q and R Matrices.
-            
+
+        //Positive Definite.
+        Eigen::Matrix<double, numberOfInputs, numberOfInputs> R;
+        //Positive Semi Definite.
+        Eigen::Matrix<double, numberOfStates, numberOfStates> Q;
+
+        //Initial Cost.
+        double cost = 0;
+
+    public:
+
+        /*
+        
+            Cost Function Algorithm:
+
+                J += X.T @ Q @ X + U.T @ R @ U
+
+        */
+        void costFunc();
+
+        Eigen::Matrix<double, numberOfInputs, numberOfStates> result();
 
 };
 
--- src/lqr.cpp
+++ src/lqr.cpp
@@ -1,3 +1,12 @@
 #include "lqr/lqr.h"
 
+template<int numberOfInputs, int numberOfOutputs, int numberOfStates>
+lqr<numberOfInputs, numberOfOutputs, numberOfStates>::lqr()
+{
+}
+
+template<int numberOfInputs, int numberOfOutputs, int numberOfStates>
+void lqr<numberOfInputs, numberOfOutputs, numberOfStates>::costFunc()
+{
+}
 
